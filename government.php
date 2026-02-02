c320afd61b1049e0966e9ee37795dc1306efc1a5
abdlhamedaloutman-debug <abdlhamedaloutman@gmail.com>
2026-02-02 20:02:36 +0100

    government.php

---
+++ government.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html><head><meta charset="UTF-8"><title>Government</title></head>
+<body style="background:#0b1324;color:white;font-family:Arial;padding:40px;">
+<h1>Royal Government</h1>
+<ul>
+<li>👑 The King</li>
+<li>🏛 Royal Council</li>
+<li>⚖ Ministry of Justice</li>
+<li>🌍 Royal Digital Foreign Office</li>
+<li>💰 Ministry of Digital Economy</li>
+</ul>
+<a href="index.php" style="color:gold;">Back to Home</a>
+</body></html>
